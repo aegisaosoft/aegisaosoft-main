c2b821fa2c32026cb4874e1df49193c916d778e4
alexander.orlov <Alexander.Orlov@huur-us.com>
2026-06-04 16:57:26 -0400

    Copyright

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2025-2026 Aegis AO Soft LLC and Alexander Orlov.
+ * 34 Middletown Ave, Atlantic Highlands, NJ 07716
+ *
+ * THIS SOFTWARE IS THE CONFIDENTIAL AND PROPRIETARY INFORMATION OF
+ * Aegis AO Soft LLC and Alexander Orlov.
+ *
+ * This code may be used, reproduced, modified, or distributed ONLY with the
+ * prior written permission of Aegis AO Soft LLC / Alexander Orlov.
+ *
+ * Author: Alexander Orlov
+ * Aegis AO Soft LLC
+ */
+
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
